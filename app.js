c7a052b42edb4f4ea4e33bbf0c33339477c69e45
beata <beata@emc2wave.com>
2019-11-22 20:14:28 -0800

    add to ItemCtrl constructor, data

--- app.js
+++ app.js
@@ -2,7 +2,35 @@
 
 // Item Controller
 const ItemCtrl = (function() {
-  
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data Structure / State
+  const data = {
+    items: [
+      {
+        id: 0,
+        name: 'Steak Dinner',
+        calories: 1200
+      },
+      {
+        id: 1,
+        name: 'Cooke',
+        calories: 400
+      },
+      {
+        id: 2,
+        name: 'Eggs',
+        calories: 500
+      }
+    ],
+    currentItems: null,
+    totalCalories: 0
+  }
 })();
 
 // UI Controller
