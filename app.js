d3d0e42d0fa45650d2407dd6d8ae3d4d12dd96ac
beata <beata@emc2wave.com>
2019-11-25 13:57:09 -0800

    add loadEventListener to init func

--- app.js
+++ app.js
@@ -101,6 +101,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Populate list with items
       UICtrl.populateItemList(items);
+
+      // Load event listener
+      loadEventListeners();
       
     }
   }
