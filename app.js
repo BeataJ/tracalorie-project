e4d2020b6c981691d44b66719a79419a2f619090
beata <beata@emc2wave.com>
2019-11-26 10:00:05 -0800

    Add item to form

--- app.js
+++ app.js
@@ -224,6 +224,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Set current item
       ItemCtrl.setCurrentItem(itemToEdit);
+
+      // Add item to form
+      UICtrl.addItemToForm();
     }
 
     e.preventDefault()
