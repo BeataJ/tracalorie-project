8a0d3b2f45b5b3dc9ba3de55f5d8b673eaa17dac
beata <beata@emc2wave.com>
2019-11-26 18:33:28 -0800

    create storeItem()

--- app.js
+++ app.js
@@ -2,8 +2,25 @@
 const StorageCtrl = (function(){
   // Public methods
   return {
-    storeItem: function() {
+    storeItem: function(item) {
+      let items = [];
+      // Check if any items in ls
+      if(localStorage.getItem('items') === null) {
+        items = [];
+        // Push new item
+        items.push(item);
+        // Set ls
+        localStorage.setItem('items', JSON.stringify(items));
+      } else {
+        // Get what is already in ls
+        items = JSON.parse(localStorage.getItem('items'));
 
+        // push new item
+        items.push(item);
+
+        // Re set ls
+        localStorage.setItem('items', JSON.stringify(items));
+      }
     }
   }
 
